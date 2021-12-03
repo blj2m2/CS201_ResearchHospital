68941329981485fdbfeded4bd77f2bf0d98f8141
bmmnfh <90420635+bmmnfh@users.noreply.github.com>
2021-12-02 19:33:29 -0600

    Update Patient.cpp

--- ResearchHospital/Patient.cpp
+++ ResearchHospital/Patient.cpp
@@ -1,1 +1,33 @@
 #include "Patient.h"
+using namespace std;
+
+Patient::Patient(){
+  first = " ";
+  last = " ";
+  ssn = " ";
+  patientQueue = {};
+}
+
+Patient::Patient(string first_t, string last_t, string ssn_t, vector<vector<string>> patientQueue){
+  first = first_t;
+  last = last_t;
+  ssn = ssn_t;
+  patientQueue = patientQueue;
+}
+
+string Patient::GetFirst(){return first;}
+string Patient::GetLast(){return last;}
+string Patient::GetSSN(){return ssn;}
+vector<vector<string>> Patient::GetPatientQueue(){return patientQueue;}
+
+void Patient::SetFirst(string first_t){first = first_t;}
+void Patient::SetLast(string last_t){last = last_t;}
+void Patient::SetSSN(string ssn_t){ssn = ssn_t;}
+void Patient::SetPatientQueue(vector<vector<string>> patientQueue_t){patientQueue = patientQueue_t;}
+
+void Patient::AddPatient(){}
+void Patient::AddCriticalPatient(){}
+void Patient::RemoveForOperation(){}
+void Patient::CancelPatient(){}
+void Patient::ListPatient(){}
+void Patient::ChangeDepartment(){}
