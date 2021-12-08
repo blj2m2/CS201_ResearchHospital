d1feef690ff0f5ed49a663943fce528599f9da4d
ls3k6 <90429544+ls3k6@users.noreply.github.com>
2021-12-07 22:42:44 -0600

    Update Patient.cpp

--- ResearchHospital/Patient.cpp
+++ ResearchHospital/Patient.cpp
@@ -28,6 +28,34 @@ void Patient::SetPatientQueue(vector<vector<Patient>> patientQueue_t){patientQue
 void Patient::AddPatient(){}
 void Patient::AddCriticalPatient(){}
 void Patient::RemoveForOperation(){}
-void Patient::CancelPatient(){}
+
+
+//void Patient::CancelPatient(){}
+//if SSN does not exist write error
+//SSN exist - print patient info and status and remove from queue
+void Patient::CancelPatient(string first_t, string last_t, string ssn_t, vector<vector<Patient>> patientQueue){
+  string ssn;
+  bool f = false;
+  int x;
+
+  cout << "Enter SSN of patient you would like to remove: ";
+  cin >> ssn_t;
+  
+  for(int i = 0; ssn_t.size(); i++){
+    if (stoi(ssn_t) == (ssn_t.at(i))){
+      x = i; 
+      f = true;
+    }
+  }
+  if(f){
+    cout << "Patient: " << ssn_t.at(x) << " " << ssn_t.at(x) << "'s appointment has been canceled." << endl;
+    ssn_t.erase(ssn_t.begin()+x);
+  }
+  else{
+    cout << "Patient " << ssn_t << " Does Not Exist" << endl;
+  }
+}
+
+
 void Patient::ListPatient(){}
 //void Patient::ChangeDepartment(){}
