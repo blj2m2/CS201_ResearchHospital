d368db10851280cc0b3cfaddd93e3305ff882c86
bmmnfh <90420635+bmmnfh@users.noreply.github.com>
2021-12-09 18:30:58 -0600

    Update Patient.cpp

--- ResearchHospital/Patient.cpp
+++ ResearchHospital/Patient.cpp
@@ -7,12 +7,14 @@ Patient::Patient(){
   first = " ";
   last = " ";
   ssn = " ";
+  status = " ";
 }
 
-//Patient::Patient(string first_t, string last_t, string ssn_t){
+//Patient::Patient(string first_t, string last_t, string ssn_t, string status){
 //  first = first_t;
 //  last = last_t;
 //  ssn = ssn_t;
+//  status = status_t;
 //}
 vector<vector<Patient>> Patient::patientQueue = {{},{},{}};
 
