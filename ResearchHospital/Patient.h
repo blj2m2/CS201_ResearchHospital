0c5aedc747b35f44b7668e8b5ae22037b48bbff2
ls3k6 <90429544+ls3k6@users.noreply.github.com>
2021-12-09 18:04:48 -0600

    Update Patient.h

--- ResearchHospital/Patient.h
+++ ResearchHospital/Patient.h
@@ -10,31 +10,35 @@ using std::vector;
 class Patient
 {
   private:
-  string first, last, ssn;
+  string first, last, ssn, status;//status refers to critical/not critical status
   
   public:
   Patient();
-  Patient(string first_t, string last_t, string ssn_t, vector<vector<Patient>> patientQueue);
-  vector<vector<Patient>> patientQueue;
+  Patient(string first_t, string last_t, string ssn_t, string status);
+
+  static vector<vector<Patient>> patientQueue;
 
   string GetFirst();
   string GetLast();
   string GetSSN();
+  string GetStatus();
   vector<vector<Patient>> GetPatientQueue();
 
   void SetFirst(string first_t);
   void SetLast(string last_t);
   void SetSSN(string ssn_t);
+  void SetStatus(string status_t);
   void SetPatientQueue(vector<vector<Patient>> patientQueue_t);
   
-  void AddPatient();
-  void AddCriticalPatient();
+  void AddPatient(string clinic);
+  void AddCriticalPatient(string clinic);
   void RemoveForOperation();
-  void CancelPatient();
-  void ListPatient(vector<Transaction> transactionQueue);
-  void ChangeDepartment();
-  
   void CancelPatient(string clinic);
+  void ListPatient();
+  //void ChangeDepartment(); function in Main
+
+  
 };
+
 
 
